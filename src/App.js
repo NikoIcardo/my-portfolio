afc8043e66ac7ee6136b212fb2e97a7190b3066c
Glagny <nikoicardo@gmail.com>
2021-07-31 15:15:19 -0600

    Building the Portfolio

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+//Niko Icardo 7/31/21
 import React from "react";
 
-export default function App() {
-  return "App";
-}
+import About from './components/About'; 
+import Contact from './components/Contact';
+import Navbar from './components/Navbar'; 
+import Projects from './components/Projects'; 
+import Skills from './components/Skills'; 
+import Testimonials from './components/Testimonials'; 
+
+
+const App = () => {
+  return (
+    <main className="text-gray-400 bg-gray-900 body-font">
+      <Navbar />
+      <About /> 
+      <Projects /> 
+      <Skills /> 
+      <Testimonials/>
+      <Contact />  
+    </main>
+  );
+}; 
+
+export default App; 
+ 

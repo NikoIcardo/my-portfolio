2bf68517f2f0decf8ab201ec49d47e0ec0002f42
Glagny <nikoicardo@gmail.com>
2021-08-03 21:37:23 -0600

    Figuring Out Netlify Form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -34,9 +34,9 @@ const Contact = () => {
       </div>
       <div className=" text-left mx-24">
         <form 
-          data-netlify="true"
+          netlify
           name="contact"
-          method="POST"
+          onSubmit={handleSubmit}
           className="lg:w-full md:w-full flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
         >
           <div className="relative mb-4">
@@ -47,6 +47,7 @@ const Contact = () => {
               type="text"
               id="name"
               name="name"
+              onChange={(e) => setName(e.target.value)}
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
             />
           </div>
@@ -58,6 +59,7 @@ const Contact = () => {
               type="email"
               id="email"
               name="email"
+              onChange={(e) => setEmail(e.target.value)}
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
             />
           </div>
@@ -70,6 +72,7 @@ const Contact = () => {
             <textarea
               id="message"
               name="message"
+              onChange={(e) => setMessage(e.target.value)}
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
             />
           </div>
